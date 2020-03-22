0b3a3ac4431e114b26496e5d5e4581be82a773b6
ChesyaWhite <vyacheslavbelyai@gmail.com>
2020-03-22 22:14:13 +0300

    init: start task "expression-calculator"

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
-
 function expressionCalculator(expr) {
-    // write your solution here
+    arr = expr.split('');
+    console.log(arr);
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == '*') {
+            arr[i] = Number(arr[i-1])*Number(arr[i+1]);
+            arr.splice(i-1, 1);
+            arr.splice(i, 1);
+            i=i-1;
+        }
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == '/') {
+            if (Number(arr[i+1]) == 0) {
+                console.log("TypeError: Division by zero.");
+            } else {
+                arr[i] = Number(arr[i-1])/Number(arr[i+1]);
+                arr.splice(i-1, 1);
+                arr.splice(i, 1);
+                i=i-1;
+            }
+        }
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == '+') {
+            arr[i] = Number(arr[i-1])+Number(arr[i+1]);
+            arr.splice(i-1, 1);
+            arr.splice(i, 1);
+            i=i-1;
+        }
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == '-') {
+            arr[i] = Number(arr[i-1])-Number(arr[i+1]);
+            arr.splice(i-1, 1);
+            arr.splice(i, 1);
+            i=i-1;
+        }
+    }
+    return Number(arr);
 }
 
 module.exports = {
